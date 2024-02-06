1e0b97cdd28d6490a5d7720b6853be1d04c71c4e
Cecilie Røgh Eik <ceciliereik@Cecilies-Air.uia.no>
2024-02-06 12:01:19 +0100

    added middleware to server.mjs

--- server.mjs
+++ server.mjs
@@ -1,6 +1,7 @@
-import express from 'express' // Express is installed using npm
+import 'dotenv/config'
+import express from 'express'; // Express is installed using npm
 import USER_API from './routes/usersRoute.mjs'; // This is where we have defined the API for working with users.
-
+import {HTTPCodes, HTTPMethods} from './modules/httpConstants.mjs';
 import SuperLogger from './modules/SuperLogger.mjs';
 // Creating an instance of the server
 const server = express();
@@ -12,21 +13,29 @@ server.set('port', port);
 //const logger = new SuperLogger();
 //server.use(logger.createAutoHTTPRequestLogger()); // Will logg all http method requests
 
-
 // Defining a folder that will contain static files.
 server.use(express.static('public'));
-
-
-
 // Telling the server to use the USER_API (all urls that uses this code will have to have the /user after the base address)
 server.use("/user", USER_API);
 
-// A get request handler example)
-server.get("/", (req, res, next) => {
 
-    req.originalUrl
+// error handling middleware
+server.use((err, req, res, next) =>{
+    SuperLogger.log(err);
 
-    res.status(200).send(JSON.stringify({ msg: "These are not the droids...." })).end();
+    const errorMap={
+        notFoundError: {message: 'Resource not found', statusCode: HTTPCodes.ClientSideErrorResponse.NotFound},
+        authenticationError: {message: 'Unauthorized', statusCode: HTTPCodes.ClientSideErrorResponse.Unauthorized},
+    };
+    const {message = 'Internal server error', statusCode = HTTPCodes.ClientSideErrorResponse.BadRequest} = errorMap[err.constructor.name] || {};
+    res.status(statusCode).json({error: message});
+});
+
+//triggering a notFoundError:
+server.get("/nonexistent", (req, res, next) =>{
+    const notFoundError = new Error('Resource not found');
+    notFoundError.name = 'notFoundError';
+    next(notFoundError);
 });
 
 // Start the server 
