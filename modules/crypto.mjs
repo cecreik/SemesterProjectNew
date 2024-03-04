71c8648c6026e039870217714ec2f7b2758c8842
Cecilie Røgh Eik <ceciliereik@Cecilies-Air.uia.no>
2024-03-04 10:07:31 +0100

    updated login page

---
+++ modules/crypto.mjs
@@ -0,0 +1,15 @@
+// crypto.mjs
+/* const { createHmac } = await import('node:crypto');
+
+export function generateHash(data, secret) {
+    const hash = createHmac('sha256', secret)
+                   .update(data)
+                   .digest('hex');
+    return hash;
+}
+
+// Usage example
+const secret = 'abcdefg';
+const hash = generateHash('I love cupcakes', secret);
+console.log(hash);
+*/
