5f9a2dfba40675b9c56d36fe916ce2ebf6358319
Cecilie Røgh Eik <ceciliereik@Cecilies-Air.uia.no>
2024-03-12 10:29:16 +0100

    fixed connection

--- modules/crypto.mjs
+++ modules/crypto.mjs
@@ -1,10 +1,14 @@
 // crypto.mjs
-const { createHmac } = await import('node:crypto');
+import { createHmac } from 'node:crypto';
 
-export function generateHash(data, secret) {
+export function generateHash(data) {
+    const secret = process.env.SECRET;
+    if (!secret) {
+        throw new Error('Secret is not defined in environment variables');
+    }
+
     const hash = createHmac('sha256', secret)
                    .update(data)
                    .digest('hex');
     return hash;
 }
-
