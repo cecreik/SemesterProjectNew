0551b1e4730f0b96d51b5cbd7d2b94e23d9884ac
Cecilie Røgh Eik <ceciliereik@Cecilies-Air.uia.no>
2024-03-12 14:57:13 +0100

    fixed user.mjs, application is up and running again

--- modules/user.mjs
+++ modules/user.mjs
@@ -22,21 +22,16 @@ class User {
 
   async getUserByEmail(email) {
     try {
-        // Implement your logic to fetch user from the database based on email
-        // For example:
-        const user = await DBManager.getUserByEmail(email);
-        if (!user) {
-            // If user not found, you can throw a custom error
-            const error = new Error('User not found');
-            error.statusCode = 404; // You can attach additional information to the error object
-            throw error;
-        }
-        return user;
-    } catch (error) {
-        // Log the error for debugging purposes
-        console.error('Error fetching user by email:', error);
-        // Rethrow the error or handle it based on your application's requirements
+      const user = await DBManager.getUserByEmail(email);
+      if (!user) {
+        const error = new Error('User not found');
+        error.statusCode = 404;
         throw error;
+      }
+      return user;
+    } catch (error) {
+      console.error('Error fetching user by email:', error);
+      throw error;
     }
   }
 }
